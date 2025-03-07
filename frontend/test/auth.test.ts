bc34498ed7faa9ba47383b24eecd37a19469f1fa
Fer Perez <ferp@protonmail.ch>
2025-03-07 13:55:59 +0100

    basic testing for auth

--- frontend/test/auth.test.ts
+++ frontend/test/auth.test.ts
@@ -1,19 +1,41 @@
-import { it, describe, expect } from "vitest";
+import { it, describe, expect, vi } from "vitest";
 import { auth$ } from "../src/core/AuthStore";
 
-// mock httpGateway within auth$
-
+auth$.gateway.post = vi.fn().mockResolvedValue({ accessToken: "token" });
 
 describe("auth", () => {
-    it("should start with no token", () => {
+    describe("token state", () => {
+        it("starts as empty string", () => {
+            expect(auth$.state.token.get()).toBe("");
+        });
+        it("can be set", () => {
+            auth$.state.token.set("token");
+            expect(auth$.state.token.get()).toBe("token");
+        });
+        it("can be unset", () => {
+            auth$.state.token.set("");
+            expect(auth$.state.token.get()).toBe("");
+        });
+    });
+    it("should login", async () => {
+        // act
+        await auth$.login("username", "password");
+        // assert
+        // check the fn was called
+        expect(auth$.gateway.post).toHaveBeenCalledWith("/login", {
+            username: "username",
+            password: "password",
+        });
+        // check the token was set
+        expect(auth$.state.token.get()).toBe("token"); //token from mock
+    });
+    it("should logout", async () => {
+        // act
+        await auth$.logout();
+        // assert
+        // check the fn was called
+        expect(auth$.gateway.post).toHaveBeenCalledWith("/logout", {});
+        // check the token was set
         expect(auth$.state.token.get()).toBe("");
     });
-    it("should set token", () => {
-        auth$.state.token.set("token");
-        expect(auth$.state.token.get()).toBe("token");
-    });
-    it("should remove token", () => {
-        auth$.state.token.set("");
-        expect(auth$.state.token.get()).toBe("");
-    });
-})
+});
